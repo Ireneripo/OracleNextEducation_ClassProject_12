38c3fe64d4bb83a4f30aa860f396ac55d99a952e
Irene Riportella <riportellairene@gmail.com>
2022-08-12 17:16:49 +0200

    playing with variables

---
+++ src/main/java/TipoVariable.java
@@ -0,0 +1,14 @@
+public class TipoVariable {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+
+        int edad = 39;
+        System.out.println(edad);
+
+        edad = 47;
+        System.out.println(edad);
+
+        edad = 46 + 88;
+        System.out.println("Mi edad es " + edad);
+    }
+}
