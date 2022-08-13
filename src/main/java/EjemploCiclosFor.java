698eb819c1b7a9a717d19743fcd620c4ae80675d
Irene Riportella <riportellairene@gmail.com>
2022-08-13 10:51:47 +0200

    working with for

---
+++ src/main/java/EjemploCiclosFor.java
@@ -0,0 +1,9 @@
+public class EjemploCiclosFor {
+    public static void main(String[] args) {
+
+        for (int contador = 0; contador <= 10; contador++) {
+            System.out.println("El contador es igual a " + contador);
+        }
+
+    }
+}
